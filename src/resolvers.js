bbc42a5ca0c6b851ce2b0cb0c1310b4b2ce29b25
Carrie Geng <carr1eg@hotmail.com>
2022-01-05 14:20:30 -0800

    update resolver

--- src/resolvers.js
+++ src/resolvers.js
@@ -44,7 +44,7 @@ const resolvers = {
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
-    durationInSeconds: ({length}) => {return length}
+    durationInSeconds: ({length}) => length
   },
   Module: {
     durationInSeconds: ({length}) => length
